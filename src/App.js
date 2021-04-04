b3317b9cbc665b5570dfde28819c9f1ef14b42f7
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2021-04-05 01:25:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,60 +3,60 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 import axios from 'axios';
-
 import {
     Typography,
     Grid,
     IconButton,
     Paper
-} from "@material-ui/core";
+} from '@material-ui/core';
 
-import FileCopyIcon from "@material-ui/icons/FileCopy";
-
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 
 function App() {
 
     const [links, setLinks] = useState([]);
 
     useEffect(() => {
-        axios.get(`https://raw.githubusercontent.com/LorenzoTinfena/LorenzoTinfena/master/server/u`)
-            .then(res => setLinks(res.data))
+        axios.get(`https://raw.githubusercontent.com/LorenzoTinfena/LorenzoTinfena/master/server/urls.json`)
+             .then(res => setLinks(res.data))
     }, [])
 
-    /*const links = [
-        { title: "primo link", url: "23270ff51881.ngrok.io" },
-        { title: "primo link", url: "23270ff51881.ngrok.io" },
-        { title: "primo link", url: "23270ff51881.ngrok.io" }
-    ];*/
-//https://raw.githubusercontent.com/LorenzoTinfena/LorenzoTinfena/master/server/urls.json
 return (
     <>
         <Paper
-            style={{ width: "50vw", padding: 10, margin: "auto", marginTop: 100 }}>
+            style={{ width: '50vw', padding: 10, margin: 'auto', marginTop: 100 }}>
             <Grid
-                justify="center"
-                alignItems="center"
+                justify='center'
+                alignItems='center'
                 container>
-                <Grid item xs="auto" style={{ margin: 10, marginBottom: 20 }}>
-                    <Typography variant="h5">I MIEI LINK</Typography>
+                <Grid item xs='auto' style={{ margin: 10, marginBottom: 20 }}>
+                    <Typography variant='h5'>I MIEI LINK</Typography>
                 </Grid>
                 {links.map(({ title, url }) => (
-                    <Grid item xs="12">
+                    <Grid item xs='12'>
                         <Grid
                             container
-                            direction="row"
-                            alignItems="center">
-                            <Grid item xs="3">
-                                <Typography align="center">{title}</Typography>
+                            direction='row'
+                            alignItems='center'>
+                            <Grid item xs='3'>
+                                <Typography align='center'>{title}</Typography>
                             </Grid>
-                            <Grid item xs="5">
-                                <Typography align="center">{url}</Typography>
+                            <Grid item xs='5'>
+                                <Typography align='center'>{url}</Typography>
                             </Grid>
-                            <Grid item xs="1">
-                                {" "}
+                            <Grid item xs='1'>
+
                                 <IconButton
                                     onClick={() => navigator.clipboard.writeText(url)}>
                                     <FileCopyIcon />
+                                </IconButton>
+                            </Grid>
+                            <Grid item xs='1'>
+
+                                <IconButton
+                                    onClick={() => window.open(`https://${url}`, '_blank')}>
+                                    <OpenInNewIcon />
                                 </IconButton>
                             </Grid>
                         </Grid>
