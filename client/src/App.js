ab31f5666cf4cd91f4bef1867548b1a459b1c168
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2021-04-08 13:59:56 +0200

    sembrerebbe buono manca la connessione al db pero

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,77 @@
+
+import './App.css';
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+import {
+    Typography,
+    Grid,
+    IconButton,
+    Paper
+} from '@material-ui/core';
+
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
+
+
+function App() {
+    
+    global.fetch = require("node-fetch");
+
+    const [links, setLinks] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/urls.json')
+        .then((res) => setLinks(res.data))
+    }, []);
+return (
+    <>
+        <Paper
+            style={{ width: '50vw', padding: 10, margin: 'auto', marginTop: 100 }}>
+            <Grid
+                justify='center'
+                alignItems='center'
+                container>
+                <Grid item xs='auto' style={{ margin: 10, marginBottom: 20 }}>
+                    <Typography variant='h5'>I MIEI LINK</Typography>
+                </Grid>
+                {links.map(({ title, url }) => (
+                    <Grid item xs='12'>
+                        <Grid
+                            container
+                            direction='row'
+                            alignItems='center'>
+                            <Grid item xs='3'>
+                                <Typography align='center'>{title}</Typography>
+                            </Grid>
+                            <Grid item xs='5'>
+                                <Typography align='center'>{url}</Typography>
+                            </Grid>
+                            <Grid item xs='1'>
+
+                                <IconButton
+                                    onClick={() => navigator.clipboard.writeText(url)}>
+                                    <FileCopyIcon />
+                                </IconButton>
+                            </Grid>
+                            <Grid item xs='1'>
+
+                                <IconButton
+                                    onClick={() => window.open(`https://${url}`, '_blank')}>
+                                    <OpenInNewIcon />
+                                </IconButton>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                ))}
+            </Grid>
+        </Paper>
+    </>
+);
+}
+
+export default App;
+/*
 // client/src/App.js
 
 import React from "react";
@@ -23,4 +97,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
--- server/index.js
+++ server/index.js
@@ -1,15 +1,39 @@
 const path = require('path');
 const express = require('express');
+const { Console } = require('console');
+var bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.get("/api/urls.json", (req, res) => {
+  res.json([
+    {title: "Youtube", url: "youtube.com"},
+    {title: "Google", url: "google.com"},
+    {title: "Hub docker", url: "hub.docker.com"}
+  ]);
 });
+
+app.post("/api/update-urls", urlencodedParser, (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+/* like
+  [
+    {"title": "Youtube", "url": "youtube.com"},
+    {"title": "Google", "url": "google.com"},
+    {"title": "Hub docker", "url": "hub.docker.com"}
+  ]
+*/
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
