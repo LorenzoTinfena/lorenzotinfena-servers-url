bdced83c5a4a46dc857cfcf465f0b4dd9e34e242
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2021-04-10 13:44:37 +0200

    kkk

--- client/src/App.js
+++ client/src/App.js
@@ -57,7 +57,7 @@ return (
                             <Grid item xs='1'>
 
                                 <IconButton
-                                    onClick={() => window.open(`https://${url}`, '_blank')}>
+                                    onClick={() => window.open(`${url}`, '_blank')}>
                                     <OpenInNewIcon />
                                 </IconButton>
                             </Grid>
@@ -71,30 +71,3 @@ return (
 }
 
 export default App;
-/*
-// client/src/App.js
-
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
